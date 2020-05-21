a3f60bb9cb4bd8d4d68ca1625e0dbc68b2d83cb1
Alice-Xiong <alicexxx0516@gmail.com>
2020-05-21 16:18:49 -0700

    working analog with screen

--- tester/src/main.cpp
+++ tester/src/main.cpp
@@ -4,7 +4,8 @@
 #define SCREEN_WIDTH 128 // OLED display width, in pixels
 #define SCREEN_HEIGHT 64 // OLED display height, in pixels
 #define OLED_RESET 	-1 // This display does not have a reset pin accessible
-#define COLLISION_PIN PA0
+#define TAPE_DETECT_PIN PA7
+#define REF_PIN PA1
 #define PROG_CONTROL 1
 
 void handle_interrupt();
@@ -14,8 +15,9 @@ Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
 
 
 void setup() {
-    // setup collision switch PA0
-    pinMode(COLLISION_PIN, INPUT_PULLUP);
+    // setup taper detecter pin PA7
+    pinMode(TAPE_DETECT_PIN, INPUT);
+    pinMode(REF_PIN, INPUT);
 
     // start up the OLED screen
     display.begin(SSD1306_SWITCHCAPVCC, 0x3C);
@@ -24,28 +26,19 @@ void setup() {
     display.clearDisplay();
     display.setTextSize(1);
     display.setTextColor(SSD1306_WHITE);
-
-    // attach interrupt
-    attachInterrupt(digitalPinToInterrupt(COLLISION_PIN), handle_interrupt, RISING);
-
 }
 
 void loop() {
-    int loopcount = 0;
     while(digitalRead(PROG_CONTROL)) {
         display.clearDisplay();
         display.setCursor(0,0);
-        display.println("Loop counter:  ");
-        display.println(loopcount);
-        display.println("Collisions:  ");
-        display.println(i);
+        display.println(analogRead(TAPE_DETECT_PIN));
         display.display();
-        loopcount++;
         delay(300);
     }
 }
 
 
 void handle_interrupt() {
-    i++;
+
 }
