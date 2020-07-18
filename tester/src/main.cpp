9765d4e1370bfec04840819c8d7487eecff2ae19
Alice-Xiong <alicexxx0516@gmail.com>
2020-07-17 18:26:15 -0700

    oled and hbridge pins

--- tester/src/main.cpp
+++ tester/src/main.cpp
@@ -1,28 +1,36 @@
 #include <Wire.h>
 #include <Adafruit_SSD1306.h>
 
+#define PROG_CONTROL 1
+
+// DC motor definitions
+#define M1_B PA_7
+#define M1_F PA_6
+#define M2_F PB_0
+#define M2_B PB_1
+#define PWM_FREQ 2000
+
+// OLED definitions
 #define SCREEN_WIDTH 128 // OLED display width, in pixels
 #define SCREEN_HEIGHT 64 // OLED display height, in pixels
 #define OLED_RESET 	-1 // This display does not have a reset pin accessible
-#define TAPE_DETECT_PIN PA7
-#define REF_PIN PA1
-#define PROG_CONTROL 1
-
-void handle_interrupt();
-volatile int i = 0;
+#define OLED_SDA PB_11
+#define OLED_SCL PB_10
 
 Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
 
-
 void setup() {
-    // setup taper detecter pin PA7
-    pinMode(TAPE_DETECT_PIN, INPUT);
-    pinMode(REF_PIN, INPUT);
+    Wire.setSCL(OLED_SCL);
+    Wire.setSDA(OLED_SDA);
+
+    // setup pwm out pins
+    pinMode(M1_F, OUTPUT);
+    pinMode(M1_B, OUTPUT);
+    pinMode(M2_F, OUTPUT);
+    pinMode(M2_B, OUTPUT);
 
     // start up the OLED screen
     display.begin(SSD1306_SWITCHCAPVCC, 0x3C);
-    display.display();
-    delay(2000);
     display.clearDisplay();
     display.setTextSize(1);
     display.setTextColor(SSD1306_WHITE);
@@ -30,20 +38,14 @@ void setup() {
 
 void loop() {
     while(digitalRead(PROG_CONTROL)) {
+        //pwm_start(M1_F, PWM_FREQ, 3000, RESOLUTION_12B_COMPARE_FORMAT);
+        //pwm_start(M2_B, PWM_FREQ, 3000, RESOLUTION_12B_COMPARE_FORMAT);
+
         display.clearDisplay();
         display.setCursor(0,0);
-        if (analogRead(TAPE_DETECT_PIN) > analogRead(REF_PIN)) {
-            display.println("Tape detected");
-        } else {
-            display.println("No tape!");
-        }
-        
+        display.println("Aloha");
         display.display();
-        delay(300);
+        delay(50);
     }
 }
 
-
-void handle_interrupt() {
-
-}
